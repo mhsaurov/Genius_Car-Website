89f510376591b9fc8bf93719e86fe21cdaa66fa1
mhsaurov <skmehedihassan58@gmail.com>
2022-11-08 01:32:38 +0600

    header and footer hasbeen done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import './App.css';
 import { RouterProvider } from 'react-router-dom';
-import { router } from '../Routes/Routes/Routes';
+import './App.css';
+import { router } from './Routes/Routes';
 
 function App() {
 
@@ -9,7 +9,7 @@ function App() {
 
 
   return (
-    <div className="App">
+    <div >
    <RouterProvider router={router}></RouterProvider>
     </div>
   );
